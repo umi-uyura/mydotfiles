3b257afa41a67007483d424e5350fb9a828705cc
Umi Uyura <umiuyura@gmail.com>
2021-09-10 16:59:16 +0900

    WSL側の更新チェックを追加

--- bin/check-update.bat
+++ bin/check-update.bat
@@ -1,27 +1,48 @@
 @echo off
 
 echo.
-echo Checking for winget updates ...
+echo ### Checking for winget updates ...
 echo.
 
 winget upgrade
 
 echo.
-echo Checking for Chocolatey updates ...
+echo ### Checking for Chocolatey updates ...
 echo.
 
 choco outdated --ignore-pinned
 
 echo.
-echo Checking for Node package updates ...
+echo ### Checking for Node package updates ...
 echo.
 
 @rem ncu -g
 
 echo.
-echo If you want to update WSL, press any key to continue . . .
+echo ----------------------------------------------------------
+echo If you want to update WSL, press any key to continue.
+echo To use sudo, you need to enter the administrator password.
+echo.
+echo If you don't need it, press Ctrl + C to quit.
+echo ----------------------------------------------------------
 echo.
 
 pause
 
-::wsl myupdate
+echo.
+echo ### In wsl world ...
+echo.
+
+
+echo.
+echo ### Checking for apt package updates ...
+echo.
+
+wsl -d Ubuntu sudo apt update
+wsl -d Ubuntu apt list --upgradable
+
+echo.
+echo ### Checking for Homebrew updates ...
+echo.
+
+wsl -d Ubuntu source $HOME/.profile; brew outdated
