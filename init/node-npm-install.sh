a7befa2affc5a7b9b13a66da2784b5b1017c1285
Umi Uyura <umiuyura@gmail.com>
2021-09-28 20:03:50 +0900

    [node] 初期グローバルインストール分

---
+++ init/node-npm-install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Web Develop
+npm install -g http-server
+npm install -g json-server
+
+# Vue.js
+npm install -g @vue/cli
+npm install -g @vue/cli-service-global
+npm install -g @vue/devtools
+
+# Linter
+npm install -g eslint
+npm install -g eslint-plugin-vue
+
+# Utilities
+npm install -g npm-check-updates
