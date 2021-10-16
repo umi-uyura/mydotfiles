b6bdfe924d1a5c0d473a84afd6f2e67f858ef19b
Umi Uyura <umiuyura@gmail.com>
2021-10-16 13:25:00 +0900

    [winget] キャプチャツールをGreenshotからFlameshotに変更

--- init/win/winget-install.bat
+++ init/win/winget-install.bat
@@ -13,8 +13,7 @@ winget install -s winget -e --id Automattic.Simplenote
 winget install -s winget -e --id Typora.Typora
 winget install -s winget -e --id JGraph.Draw
 winget install -s winget -e --id XMind.XMind2020
-winget install -s winget -e --id Greenshot.Greenshot
-@REM or https://app.prntscr.com/en/index.html
+winget install -s winget -e --id Flameshot.Flameshot
 
 winget install -s winget -e --id Microsoft.OpenJDK.11
 winget install -s winget -e --id GitHub.cli
