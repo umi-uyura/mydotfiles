1e0cb2751d4ea6f49eb4f0053fd1de0b3c4bc307
Umi Uyura <umiuyura@gmail.com>
2022-02-19 10:31:50 +0900

    [winget] FastCopyを追加

--- init/win/winget-install.bat
+++ init/win/winget-install.bat
@@ -9,6 +9,7 @@ winget install -s winget -e --id Lexikos.AutoHotkey
 winget install -s winget -e --id 7zip.7zip
 winget install -s winget -e --id GnuWin32.Zip
 winget install -s winget -e --id Flow-Launcher.Flow-Launcher
+winget install -s winget -e --id FastCopy.FastCopy
 winget install -s winget -e --id File-New-Project.EarTrumpet
 
 winget install -s winget -e --id Adobe.AdobeAcrobatReaderDC
