7533220de4b416fca9e67adccb035abee48c5d9b
Umi Uyura <umiuyura@gmail.com>
2022-05-14 13:19:59 +0900

    [npm] 共通でインストールするものと個別のものを分離

--- init/node-npm-globals-install.sh
+++ init/node-npm-globals-install.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-cat $(dirname ${0})/node-npm-globals.txt | tr -d \\r | xargs npm install -g
+cat $(dirname ${0})/node-npm-globals-common.txt | tr -d \\r | xargs npm install -g
+
+#
+# Install only on WSL
+#
+npm install -g surge
+npm install -g tldr
