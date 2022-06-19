81758e446d5904a29d9000195c7f5f7a3dfbce76
Umi Uyura <umiuyura@gmail.com>
2022-06-19 18:21:20 +0900

    [golang] JSON展開ツールgronを追加

--- init/golang-go-install-asdf.sh
+++ init/golang-go-install-asdf.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-go install github.com/isacikgoz/tldr/cmd/tldr@latest
+#
+# Refer:
+#   dots/.default-golang-pkgs
+#
+
+# go install github.com/isacikgoz/tldr/cmd/tldr@latest  # Trying the Node.js version
 go install github.com/cheat/cheat/cmd/cheat@latest
+go install github.com/tomnomnom/gron@latest
 
 asdf reshim golang
