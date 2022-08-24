6f3ca6ae57c593a5a139d1502b1589f212458eea
Umi Uyura <umiuyura@gmail.com>
2022-08-25 01:00:02 +0900

    wingetインストール用バッチは廃止

--- init/win/pre/winget-pre.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-winget install -s winget -e --id Microsoft.WindowsTerminal
-winget install -s winget -e --id Git.Git
-winget install -s winget -e --id Microsoft.VisualStudioCode
-winget install -s winget -e --id Google.Chrome
-winget install -s winget -e --id Dropbox.Dropbox
-winget install -s winget -e --id KeePassXCTeam.KeePassXC
