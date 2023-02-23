e5eb1765cf54d22843f2ce37529fb80cf47b1190
Umi Uyura <umiuyura@gmail.com>
2023-02-24 00:27:46 +0900

    [macOS] バッテリー残量表記の変更方法をBig Surで動くようにした

--- init/macos/osx-defaults.sh
+++ init/macos/osx-defaults.sh
@@ -13,7 +13,7 @@ sudo nvram SystemAudioVolume=%05
 # フルキーボードアクセスを有効
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-# Fuキーを標準のファンクションキーとして使用
+# Fnキーを標準のファンクションキーとして使用
 defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
 
 # キーのリピート
@@ -47,7 +47,7 @@ defaults -currentHost write -globalDomain AppleFontSmoothing -int 2
 
 
 # バッテリー残量を％表記に
-defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist BatteryShowPercentage -bool true
 
 # 日付と時刻のフォーマット（24時間表示、秒表示あり、日付・曜日を表示）
 defaults write com.apple.menuextra.clock DateFormat -string "M\u6708d\u65e5(EEE)  H:mm:ss"
