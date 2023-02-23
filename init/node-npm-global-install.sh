40254fa54a6ccdb2d4a28964d9d08bf97f363650
Umi Uyura <umiuyura@gmail.com>
2023-02-24 00:31:28 +0900

    Fix shellcheck warning

--- init/node-npm-global-install.sh
+++ init/node-npm-global-install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cat $(dirname ${0})/node-npm-global-common.txt | tr -d \\r | xargs npm install --location=global
+< "$(dirname "${0}")/node-npm-global-common.txt" tr -d \\r | xargs npm install --location=global
 
 #
-# Install only on WSL
+# Install only on WSL/Linux/macOS
 #
 npm install -g surge
 npm install -g tldr
