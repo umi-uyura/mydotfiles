9af221dbf14db6238e191a68ed8d0a1c8744de82
Umi Uyura <umiuyura@gmail.com>
2023-03-18 21:51:34 +0900

    [Windows] .gitconfigのシンボリックリンク作成時にすでに存在していたら _local に変更する

--- dots/_setup.bat
+++ dots/_setup.bat
@@ -1,12 +1,15 @@
 @echo off
 
-pushd %~dp0
+PUSHD %~dp0
 
-echo "Copy dotfiles to HOME (USERPROFILE)"
+ECHO "Copy dotfiles to HOME (USERPROFILE)"
 
-mklink %USERPROFILE%\.wslconfig %USERPROFILE%\dotfiles\dots\win\.wslconfig
-mklink %USERPROFILE%\.gitconfig %USERPROFILE%\dotfiles\dots\.gitconfig
-mklink %USERPROFILE%\.gitignore %USERPROFILE%\dotfiles\dots\.gitignore
-mklink %USERPROFILE%\.gitmessage.txt %USERPROFILE%\dotfiles\dots\.gitmessage.txt
+MKLINK %USERPROFILE%\.wslconfig %USERPROFILE%\dotfiles\dots\win\.wslconfig
+IF EXIST %USERPROFILE%\.gitconfig (
+   MOVE /Y %USERPROFILE%\.gitconfig %USERPROFILE%\.gitconfig_local
+)
+MKLINK %USERPROFILE%\.gitconfig %USERPROFILE%\dotfiles\dots\.gitconfig
+MKLINK %USERPROFILE%\.gitignore %USERPROFILE%\dotfiles\dots\.gitignore
+MKLINK %USERPROFILE%\.gitmessage.txt %USERPROFILE%\dotfiles\dots\.gitmessage.txt
 
-popd
+POPD
