1768be59e1e2d40f871965dcc2e2ebdb4ecbe840
Umi Uyura <umiuyura@gmail.com>
2024-01-30 22:59:48 +0900

    [Windows] fzf設定追加

--- shell/mystartup.cmd
+++ shell/mystartup.cmd
@@ -13,6 +13,13 @@ SET MYSTARTUP_INIT=OK
 @REM Register doskey macros
 doskey /macrofile=%USERPROFILE%\dotfiles\shell\doskey-alias.macros
 
+@REM Setup fzf
+WHERE fzf >nul 2>nul
+IF %ERRORLEVEL% EQU 0 (
+   SET FZF_DEFAULT_COMMAND=rg --files . 2> nul
+   SET FZF_DEFAULT_OPTS="--layout=reverse"
+)
+
 @REM Setup fnm
 WHERE fnm >nul 2>nul
 IF %ERRORLEVEL% EQU 0 (
